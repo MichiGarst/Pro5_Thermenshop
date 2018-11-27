35336c01fb0a496398fb2565886760e154500e02
petra-kreuzhuber <petra-kreuzhuber@web.de>
2018-11-27 13:46:01 +0100

    about angepasst

--- Thermeninsel/about.php
+++ Thermeninsel/about.php
@@ -1,75 +1,82 @@
 <!DOCTYPE html>
 <html lang="de">
 <head>
-	<title>Über uns</title>
-	<meta charset="utf-8">
-	<meta name="description" content="Thermenshop Bad Füssing Kontakt">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" type="text/css" href="styles/accessibility.css">
+    <title>Über uns</title>
+    <meta charset="utf-8">
+    <meta name="description" content="Thermenshop Bad Füssing Kontakt">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" href="styles/accessibility.css">
+    <link rel="stylesheet" type="text/css" href="styles/aboutstyle.css">
 
-	<!-- Bulma einbindung -->
+    <!-- Bulma einbindung -->
     <?php
     include("Components/BulmaEinbindung.html");
     ?>
-	<!-- Andere Stylesheets -->
+    <!-- Andere Stylesheets -->
 </head>
 <body>
 
 <div class="content">
 
-	<!-- Header -->
+    <!-- Header -->
 
     <?php
     include("Components/Header.html");
     ?>
 
-	<!-- Menu -->
-	<div class="is-centered">
-		<img src="images/fashybild1.jpg" alt="" width="100%" id="thermePic">
-	</div>
-	<!-- Contact -->
+    <!-- Menu -->
+    <div class="is-centered">
+        <img src="images/fashybild1.jpg" alt="" width="100%" id="thermePic">
+    </div>
+    <!-- Contact -->
 
-	<div id="main" tabindex="-1" class="has-text-centered">
-		<h1>Die Thermeninsel</h1>
-		<p class="text">
-			Die Seele der Thermeninsel sind unsere Mitarbeiter, die mit viel Herz, Erfahrung und Tatendrang ihre Arbeit erledigen.
-			Mit unserer jahrenlagen Erfahrung beraten wir dich gerne und helfen dir das richtige zu finden.
-		</p>
+    <div id="main" tabindex="-1" class="has-text-centered">
+        <div id="welcome">
+            <h1>Die Thermeninsel</h1>
+            <p class="text">
+                Die Seele der Thermeninsel sind unsere Mitarbeiter, die mit viel Herz, Erfahrung und Tatendrang ihre Arbeit
+                erledigen.
+                Mit unserer jahrenlagen Erfahrung beraten wir dich gerne und helfen dir das richtige zu finden.
+            </p>
+        </div>
 
-		<h2>Das Team</h2>
-		<div tabindex="-1" class="columns is-multiline is-centered is-mobile">
-			<div class="column is-narrow has-text-centered">
-				<img src="images/Person2.png" alt="" class="team">
-				<p>
-					<strong>Karl Lorenzer</strong><br>Inhaber, Geschäftsführer
-				</p>
-			</div>
-			<div class="column is-narrow has-text-centered">
-				<img src="images/Person2.png" alt="" class="team">
-				<p>
-					<strong>Theresa Dichtleder</strong><br>Mitarbeiterin
-				</p>
-			</div>
-			<div class="column is-narrow has-text-centered">
-				<img src="images/Person2.png" alt="" class="team">
-				<p>
-					<strong>Mirjam Jobst</strong><br>Mitarbeiterin
-				</p>
-			</div>
-			<div class="column is-narrow has-text-centered">
-				<img src="images/Person2.png" alt="" class="team">
-				<p>
-					<strong>Petra Kreuzhuber</strong><br>Mitarbeiterin
-				</p>
-			</div>
-			<div class="column is-narrow has-text-centered">
-				<img src="images/Person2.png" alt="" class="team">
-				<p>
-					<strong>Katharina Graf</strong><br>Mitarbeiterin
-				</p>
-			</div>
-		</div>
-	</div>
+        <div id="team">
+            <h2>Das Team</h2>
+            <div tabindex="-1" class="columns is-multiline is-centered is-mobile">
+                <div class="column is-narrow has-text-centered">
+                    <img src="images/Person2.png" alt="" class="team">
+                    <p>
+                        <strong>Karl Lorenzer</strong><br>Inhaber, Geschäftsführer
+                    </p>
+                </div>
+                <div class="column is-narrow has-text-centered">
+                    <img src="images/Person2.png" alt="" class="team">
+                    <p>
+                        <strong>Theresa Dichtleder</strong><br>Mitarbeiterin
+                    </p>
+                </div>
+                <div class="column is-narrow has-text-centered">
+                    <img src="images/Person2.png" alt="" class="team">
+                    <p>
+                        <strong>Mirjam Jobst</strong><br>Mitarbeiterin
+                    </p>
+                </div>
+                <div class="column is-narrow has-text-centered">
+                    <img src="images/Person2.png" alt="" class="team">
+                    <p>
+                        <strong>Petra Kreuzhuber</strong><br>Mitarbeiterin
+                    </p>
+                </div>
+                <div class="column is-narrow has-text-centered">
+                    <img src="images/Person2.png" alt="" class="team">
+                    <p>
+                        <strong>Katharina Graf</strong><br>Mitarbeiterin
+                    </p>
+                </div>
+            </div>
+        </div>
+
+    </div>
 
     <?php
     include("Components/Footer.html");
