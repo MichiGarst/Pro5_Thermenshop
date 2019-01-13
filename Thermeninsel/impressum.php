0e711357b82e2dc0a6880ebd2d896f2048279f47
petra-kreuzhuber <petra-kreuzhuber@web.de>
2019-01-13 19:40:10 +0100

    impressum

--- Thermeninsel/impressum.php
+++ Thermeninsel/impressum.php
@@ -58,6 +58,62 @@
 
 
     </div>
+    <div id="impressum">
+        <p>
+            Angaben für www.thermeninsel.de, www.lorenzer.com und www.thermenparadies.de gemäß § 5 TMG:
+<br>
+            <br>
+            Karl Lorenzer
+            Thermeninsel Bad Füssing - Therme Eins
+            Kurallee 1
+            94072 Bad Füssing
+            <br>
+            <strong> Kontakt:</strong>
+            <br>
+            Telefon: +49 (0)8531 978706
+            Telefax: +49 (0)8531 978708
+            E-Mail: info@thermeninsel.de
+            <br>
+          <strong>Umsatzsteuer:</strong>
+            <br>
+            Umsatzsteuer-Identifikationsnummer gemäß §27 a Umsatzsteuergesetz:
+            DE128394510
+            <br>
+           <strong>Wer wird sind:</strong>
+            <br>
+            Die Thermeninsel Bad Füssing im Eingangsbereich der Therme 1 in der Kurallee, einer der Haupstrassen in Bad Füssing, ist ihr Spezialist für Bademoden, Badeanzüge, Badehosen, Bikinis, Badeschuhe, Handtücher, Saunatücher, Badetücher, Wellness - Artikel, Kosmetik - Artikel, Zeitungen, Zeitschriften, Pressefachhandel, Bücher sowie hochwertiger Kaffee Produkte. Darunter zählen feinschmecker Kaffee von I.O. aus dem Hause Azul. An unserer Espresso Bar servieren wir 365 Tage im Jahr zu den Öffnungszeiten der Therme 1 Bad Füssing: Espresso, Cappucino, Latte Macchiato, Tee und heiße Schololade. Zu unseren Lieferanten gehören unter anderem Mode - Hersteller wie Vossen, Volin, Speedo, Benetton, Otto Werner, Fashy, Betty Barclay, Palm Beach, u.v.a.m. - Wir bieten Mode mit exklusiven italienischen Designs sowie Bademoden namhafter Hersteller an. Viele darunter Made in Germany und zu fairen Angeboten und Konditionen. Das Geschäft ist Inhabergeführt und existiert in dieser Form seit Anfang 2003.
+            <br>
+                <strong>Streitschlichtung:</strong>
+            <br>
+            Wir sind nicht bereit oder verpflichtet, an Streitbeilegungsverfahren vor einer Verbraucherschlichtungsstelle teilzunehmen.
+            <br>
+                    <strong> Haftung für Inhalte:</strong>
+            <br>
+            Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde Informationen zu überwachen oder nach Umständen zu forschen, die auf eine rechtswidrige Tätigkeit hinweisen.
+            <br>
+            Verpflichtungen zur Entfernung oder Sperrung der Nutzung von Informationen nach den allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche Haftung ist jedoch erst ab dem Zeitpunkt der Kenntnis einer konkreten Rechtsverletzung möglich. Bei Bekanntwerden von entsprechenden Rechtsverletzungen werden wir diese Inhalte umgehend entfernen.
+            <br>
+                        <strong>  Haftung für Links:</strong>
+            <br>
+            Unser Angebot enthält Links zu externen Websites Dritter, auf deren Inhalte wir keinen Einfluss haben. Deshalb können wir für diese fremden Inhalte auch keine Gewähr übernehmen. Für die Inhalte der verlinkten Seiten ist stets der jeweilige Anbieter oder Betreiber der Seiten verantwortlich. Die verlinkten Seiten wurden zum Zeitpunkt der Verlinkung auf mögliche Rechtsverstöße überprüft. Rechtswidrige Inhalte waren zum Zeitpunkt der Verlinkung nicht erkennbar.
+            <br>
+            Eine permanente inhaltliche Kontrolle der verlinkten Seiten ist jedoch ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht zumutbar. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Links umgehend entfernen.
+            <br>
+                            <strong>  Urheberrecht:</strong>
+            <br>
+            Die durch die Seitenbetreiber erstellten Inhalte und Werke auf diesen Seiten unterliegen dem deutschen Urheberrecht. Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der Verwertung außerhalb der Grenzen des Urheberrechtes bedürfen der schriftlichen Zustimmung des jeweiligen Autors bzw. Erstellers. Downloads und Kopien dieser Seite sind nur für den privaten, nicht kommerziellen Gebrauch gestattet.
+            <br>
+            Soweit die Inhalte auf dieser Seite nicht vom Betreiber erstellt wurden, werden die Urheberrechte Dritter beachtet. Insbesondere werden Inhalte Dritter als solche gekennzeichnet. Sollten Sie trotzdem auf eine Urheberrechtsverletzung aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte umgehend entfernen.
+            <br>
+                                <strong>   Photocredits, Quellenangaben für verwendete Bilder und Grafiken:</strong>
+            <br>
+            Eigene<br>
+            Fashy GmbH <br>
+            VOSSEN GmbH & Co. KG<br>
+
+
+        </p>
+    </div>
 
     <?php
     include("Components/Footer.html");
