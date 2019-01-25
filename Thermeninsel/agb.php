47517ea291f68d4ed5b1d1bad21ac73207cf7ae8
petra-kreuzhuber <petra-kreuzhuber@web.de>
2019-01-25 08:05:57 +0100

    AGBs neu formatiert

--- Thermeninsel/agb.php
+++ Thermeninsel/agb.php
@@ -29,8 +29,8 @@
     <div id="agb">
 
 <p>
-Geltung der Allgemeinen Geschäftsbedingungen:
-<br>
+<h3>Geltung der Allgemeinen Geschäftsbedingungen:
+        </h3><br>
 a) Lieferungen und Leistungen erfolgen ausschließlich aufgrund der nachstehenden Allgemeinen Geschäfts- und Lieferbedingungen. Diese sind Bestandteil aller  abgeschlossenen Verträge und gelten auch für alle künftigen Geschäftsbeziehungen, auch wenn sie nicht ausdrücklich vereinbart werden.
     <br>
 b) Mit der Erteilung des Auftrags, spätestens jedoch mit der Entgegennahme der Lieferung oder Leistung werden diese Geschäfts- und Lieferbedingungen durch den  Kunden anerkannt.
@@ -39,7 +39,7 @@ c) Entgegenstehende Geschäftsbedingungen oder abweichende Gegenbestätigungen d
     <br>
     <br>
     <br>
-Rückgabebelehrung – Rückgaberecht:
+<h3>Rückgabebelehrung – Rückgaberecht:</h3>
     <br>
 Sie können die erhaltene Ware ohne Angabe von Gründen innerhalb von zwei Wochen durch Rücksendung der Ware zurückgeben. Die Frist beginnt frühestens mit  Erhalt der Ware und dieser Belehrung.
     <br>
@@ -60,14 +60,14 @@ Nach Eingang Ihres Widerrufs sind wir verpflichtet, eventuelle Zahlungen zurück
     <br><br>
 
     <br>
-Rückgabefolgen
+<h3>Rückgabefolgen</h3>
     <br>
 Im Falle einer wirksamen Rückgabe sind die beiderseits empfangenen Leistungen zurückzugewähren und ggf. gezogene Nutzungen (z. B. Gebrauchsvorteile)  herauszugeben. Bei einer Verschlechterung der Ware kann Wertersatz verlangt werden. Dies gilt nicht, wenn die Verschlechterung der Ware ausschließlich auf  deren Prüfung - wie sie Ihnen etwa im Ladengeschäft möglich gewesen wäre - zurückzuführen ist. Im Übrigen können Sie die Wertersatzpflicht vermeiden, indem  Sie die Ware nicht wie ein Eigentümer in Gebrauch nehmen und alles unterlassen, was deren Wert beeinträchtigt.
 Bitte haben Sie Verständnis dafür, daß getragene bzw.  verschmutzte Waren aus hygienischen Gründen vom Umtausch ausgeschlossen sind! Sofern die Ware  nicht in Originalverpackung und unbenutzt ist, ist sie von der Rückgabe ausgenommen.
     <br>
     <br>
     <br>
-Sach- und Rechtsmängel
+<h3>Sach- und Rechtsmängel</h3>
     <br>
 Wir kontrollieren jeden Warenversand auf einwandfreien Zustand der Ware. Sollten Sie dennoch Grund zur Beanstandung haben, richten Sie diese bitte an:
     <br>
@@ -86,7 +86,7 @@ Für Sach- und Rechtsmängel gelten die gesetzlichen Vorschriften. Die gesetzlic
     <br>
     <br><br>
 
-Zahlungsmöglichkeiten
+<h3>Zahlungsmöglichkeiten</h3>
     <br>
 Wir akzeptieren folgende Zahlungsmöglichkeiten
 -Vorkasse durch Überweisung durch den Käufer
@@ -96,44 +96,44 @@ Wir akzeptieren folgende Zahlungsmöglichkeiten
     <br>
     <br>
     <br>
-Lieferungen und Preisangaben
+<h3>Lieferungen und Preisangaben</h3>
     <br>
 Die Auslieferung der bestellten/gekauften Ware erfolgt nach Möglichkeit am nächsten Werktag nach Zahlungseingang, wenn die Zahlung bis 12:00 Uhr mittags  gebucht wurde. Bei Kartenzahlung bzw. via dem angebotenen Zahlungsmöglichkeiten gilt ebenfalls der Zahlungseingang auf dem entsprechenden Konto bei Paypal.  Alle angegebenen Warenpreise enthalten die gesetzliche Mehrwehrtsteuer von 16 %. Sie erhalten die bestellte Ware in der Regel innerhalb von 5-7 Werktagen, in  seltenen Ausnahmefällen kann die Lieferung auch bis zu 10 Tagen dauern. Der Versand erfolgt – je nach Bestellmenge - als Päckchen oder Paket mit DHL  (Deutsche Post).Die Lieferadresse kann auf Wunsch von der Bestell-Adresse abweichen.
     <br>
     <br><br>
 
-Versandkosten
+<h3>Versandkosten</h3>
     <br>
 Die Versandkosten für die einzelnen Artikel sind einzeln individuell berechnet und sind jeweils angegeben. Mehrere Artikel werden nach Möglichkeit zusammen  versendet und Versandkosten fallen nur ein mal an. Bei Bestellungen aus dem Ausland teilen wir Ihnen die Versandkosten gesondert mit.
     <br>
     <br>
     <br>
-Beanstandungen
+<h3>Beanstandungen</h3>
     <br>
 Beanstandungen wegen Lieferumfang, Sachmängeln, Falschlieferungen und Mengenabweichungen sind, soweit diese durch zumutbare Untersuchungen feststellbar  sind, unverzüglich, spätestens jedoch binnen einer Woche nach Erhalt der Ware schriftlich oder per E-Mail geltend zu machen. Bei berechtigten Beanstandungen  übernimmt Pinocchio-Kindermode sämtliche Transportkosten vom Kunden zu Pinocchio-Kindermode und zurück. Ist im Falle des Umtausches der Ware auch die  zweite Ersatzlieferung mangelhaft, so steht dem Käufer das Recht auf Wandlung oder Minderung zu.
     <br>
     <br>
     <br>
-Speicherung des Vertragstextes
+<h3>Speicherung des Vertragstextes</h3>
     <br>
 Der Vertragstext Ihrer Bestellung wird bei uns nicht gespeichert und kann nach Abschluss des Bestellvorgangs nicht mehr abgerufen werden.
 Sie können die Bestelldaten aber unmittelbar nach dem Abschicken ausdrucken.
     <br>
     <br>
     <br>
-Eigentumsvorbehalt
+<h3>Eigentumsvorbehalt</h3>
     <br>
 Bis zur vollständigen Bezahlung bleibt die Ware Eigentum der Firma Thermeninsel Lorenzer, Bad Füssing.
     <br>
     <br>
     <br>
-Gerichtsstand
+<h3>Gerichtsstand</h3>
     <br>
 Erfüllungsort und Gerichtsstand für alle aus dem Vertragsverhältnis entstehenden Ansprüche und Rechtsstreitigkeiten ist das Amtsgericht in Passau mit  Zweigstelle Rotthalmünster/Ndb..
     <br>
     <br>
     <br>
-Datenschutz
+<h3>Datenschutz</h3>
     <br>
 Personenbezogene Daten werden vertraulich behandelt. Zur Auftragsabwicklung speichern wir die personenbezogenen Daten und geben sie zu diesem Zweck  gegebenenfalls an Dritte weiter. Eine Weitergabe von Kundendaten zu Werbezwecken findet nicht statt.
     <br>
